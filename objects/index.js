e3ae7cd924b7138093f70e5ecda0c1e399acacea
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-11-23 06:46:29 +0700

    Add Object Material

---
+++ objects/index.js
@@ -0,0 +1,51 @@
+// Objects
+// kumpulan pasangan key-value yang digunakan untuk menyimpan data dalam bentuk asosiatif
+
+// const siswa = {
+//     nama: "Adi",
+//     umur: "16",
+//     alamat: {
+//         jalan: "Jl. Gelatik Raya, No 93",
+//         kota: "Tangerang Selatan",
+//         provinsi: "Banten"
+//     }
+// }
+
+// console.log(siswa.nama);
+// console.log(siswa.umur);
+// console.log(siswa.alamat.kota);
+
+// Membuat Object pada Javascript
+// 1 Object Literal
+// const siswa = {
+//   nama: "Adi",
+//   umur: "100",
+//   alamat: {
+//     jalan: "Jalan, jalanan",
+//     kota: "Jakarta",
+//     provinsi: "Jawa",
+//   },
+// };
+
+// 2 Function Declaration
+// function buatObjekSiswa(nama, umur, jurusan) {
+//     let siswa = {};
+//     siswa.nama = nama;
+//     siswa.umur = umur;
+//     siswa.jurusan = jurusan;
+
+//     return siswa;
+// }
+
+// let siswa2 = buatObjekSiswa("Adi", 16, "RPL");
+// console.log(siswa2)
+
+// 3 Constructor
+// function Siswa(nama, umur, jurusan) {
+//   this.nama = nama;
+//   this.umur = umur;
+//   this.jurusan = jurusan;
+// }
+
+// let siswa2 = new Siswa("Adi", 16, "RPL");
+// console.log(siswa2);
